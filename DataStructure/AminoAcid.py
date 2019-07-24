ebe5c8ef6e865429a0e0fb635e0246b6e2190243
Ryo Yamamoto <ryo10244201@berkeley.edu>
2019-07-23 21:00:22 -0700

    added some new attributes

--- DataStructure/AminoAcid.py
+++ DataStructure/AminoAcid.py
@@ -1,11 +1,14 @@
-
+from Protein import Protein
 class AminoAcid:
 
-    def __init__(self, parent, name, id, atoms, coordinates, SS):
+    def __init__(self, parent=Protein(), name="", id=0, atoms=[], N=(0, 0, 0), C_alpha=(0, 0, 0), C_dash=(0, 0, 0), coordinates=[], SS=""):
         self.parent = parent
         self.name = name
         self.id = id
         self.atoms = atoms
+        self.N = N
+        self.C_alpha = C_alpha
+        self.C_dash = C_dash
         self.coordinates = coordinates
         self.SS = SS
 
--- DataStructure/Protein.py
+++ DataStructure/Protein.py
@@ -2,10 +2,11 @@ import numpy as np
 
 class Protein:
 
-    def __init__ (self, name:str="", aminoacids:list=[], lenseq:int=0):
+    def __init__ (self, name:str="", aminoacids:list=[], lenseq:int=0, PSSM:list=[]):
         self.name = name
         self.aminoacids = aminoacids
         self.lenseq = lenseq
+        self.PSSM = psssm
     
     def find(self, id):
         return self.aminoacids[id]
